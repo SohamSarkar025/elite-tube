65c0396e02487f8a441eb7aca4fa57359d53eb55
Soham Sarkar <srutidhara153@gmail.com>
2025-08-29 16:11:02 +0530

    Authentication Complete

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,17 +1,27 @@
-
 import { UserCircleIcon } from "lucide-react";
 
-import { Button} from "@/components/ui/button";
-
-export const AuthButton = ()=>{
-    //TODO: Add Different auth states
-    return (
-        <Button variant="outline"
-        className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20
+import { Button } from "@/components/ui/button";
+import { UserButton, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
+export const AuthButton = () => {
+  //TODO: Add Different auth states
+  return (
+    <>
+      <SignedIn>
+        <UserButton />
+        {/* Add menu items for studios and User profile */}
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20
          rounded-full shadow-none"
-        >
+          >
             <UserCircleIcon />
             Sign in
-        </Button> 
-    )
-}
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
+  );
+};
