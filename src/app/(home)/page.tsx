7f198cef19f137013f1b347abf27bd990ecc8753
Soham Sarkar <srutidhara153@gmail.com>
2025-08-29 15:31:07 +0530

    Basic Layout Complete

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return(
+    <div>
+      I will load videos in future
+    </div>
+  )
+}
+
+
+//hii i am ramkrishna I am trying to make changes in this file
+//This is Soham Merge Reuquest check.
