71502ed97541aec7517531e02d483e55ad520c19
Soham Sarkar <srutidhara153@gmail.com>
2025-08-30 11:33:33 +0530

    Day 03 Starting Set up

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,6 @@
-
 export default function Home() {
-  return(
-    <div>
-      I will load videos in future
-    </div>
-  )
+  return <div>I will load videos in future ...</div>;
 }
-
 
 //hii i am ramkrishna I am trying to make changes in this file
 //This is Soham Merge Reuquest check.
