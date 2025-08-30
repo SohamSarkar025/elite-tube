698b5608992da5e95cb16fa05fb214c51c086f98
Soham Sarkar <srutidhara153@gmail.com>
2025-08-30 19:30:40 +0530

    TRPC Setup 02

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,19 @@
-export default function Home() {
-  return <div>I will load videos in future ...</div>;
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Antonio" });
+
+  return (
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error...</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 }
 
 //hii i am ramkrishna I am trying to make changes in this file
