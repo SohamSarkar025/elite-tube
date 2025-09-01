e6772ecfa0302952ac552ebac25171ddde9f0c35
Soham Sarkar <srutidhara153@gmail.com>
2025-09-01 19:59:23 +0530

    Video Uploader Added (Mux Integration)

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,24 +1,42 @@
 "use client";
+import { ResponsiveModal } from "@/components/responsive-dialog";
 import { Button } from "@/components/ui/button";
 import { trpc } from "@/trpc/client";
 import { Loader2Icon, PlusIcon } from "lucide-react";
 import { toast } from "sonner";
 
 export const StudioUploadModal = () => {
-  const utils=trpc.useUtils();
-  const create=trpc.videos.create.useMutation({
-    onSuccess:()=>{
-      toast.success("Video Created")
+  const utils = trpc.useUtils();
+  const create = trpc.videos.create.useMutation({
+    onSuccess: () => {
+      toast.success("Video Created");
       utils.studio.getMany.invalidate();
     },
-    onError:()=>{
-      toast.error("Something went wrong")
-    }
+    onError: () => {
+      toast.error("Something went wrong");
+    },
   });
   return (
-    <Button variant="secondary" onClick={()=>create.mutate()} disabled={create.isPending}>
-      {create.isPending ? <Loader2Icon className="animate-spin"/>:<PlusIcon />}
-      Create
-    </Button>
+    <>
+      <ResponsiveModal
+        title="Upload a video"
+        open={!!create.data}
+        onOpenChange={() => create.reset()}
+      >
+        <p>This will be an uploader</p>
+      </ResponsiveModal>
+      <Button
+        variant="secondary"
+        onClick={() => create.mutate()}
+        disabled={create.isPending}
+      >
+        {create.isPending ? (
+          <Loader2Icon className="animate-spin" />
+        ) : (
+          <PlusIcon />
+        )}
+        Create
+      </Button>
+    </>
   );
 };
