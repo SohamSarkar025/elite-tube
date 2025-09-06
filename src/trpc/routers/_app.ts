a1da9f17a1a6809ee0bab80ce9e184ae972767ed
Soham Sarkar <srutidhara153@gmail.com>
2025-09-06 11:42:46 +0530

    Subscription Almost Done

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,12 +4,14 @@ import { studioRouter } from '@/modules/studio/server/procedures';
 import { videosRouter } from '@/modules/videos/server/procedures';
 import { videoViewsRouter } from '@/modules/video-views/server/procedures';
 import { videoReactionsRouter } from '@/modules/video-reactions/server/procedures';
+import { subscriptionRouter } from '@/modules/subscriptions/server/procedures';
 export const appRouter = createTRPCRouter({
   studio:studioRouter,
   categories: categoriesRouter,
   videos:videosRouter,
   videoViews:videoViewsRouter,
   videoReactions:videoReactionsRouter,
+  subscriptions: subscriptionRouter,
 });
 
 
