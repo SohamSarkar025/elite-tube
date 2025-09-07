f07aa21abaa99abaf87e95dd08744bb4aa23ec2c
Soham Sarkar <srutidhara153@gmail.com>
2025-09-07 22:35:56 +0530

    Suggestions Section Skeleton

--- src/modules/videos/ui/sections/suggestions-section.tsx
+++ src/modules/videos/ui/sections/suggestions-section.tsx
@@ -2,52 +2,89 @@
 
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
-import { VideoRowCard } from "../components/video-row-card";
-import { VideoGridCard } from "../components/video-grid-card";
+import {
+  VideoRowCard,
+  VideoRowCardSkeleton,
+} from "../components/video-row-card";
+import {
+  VideoGridCard,
+  VideoGridCardSkeleton,
+} from "../components/video-grid-card";
 import { InfiniteScroll } from "@/components/infinite-scroll";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-interface SuggestionSectionProps{
-  videoId:string;
-  isManual?:boolean;
-};
+interface SuggestionSectionProps {
+  videoId: string;
+  isManual?: boolean;
+}
 
-
-export const SuggestionSection = ({
+export const SuggestionsSection = ({
   videoId,
   isManual,
-}:SuggestionSectionProps) => {
-const [suggestions,query]=trpc.suggestions.getMany.useSuspenseInfiniteQuery({
-  videoId,
-  limit:DEFAULT_LIMIT,
-},{
-  getNextPageParam:(lastPage)=>lastPage.nextCursor,
-});
+}: SuggestionSectionProps) => {
+  return (
+    <Suspense fallback={<SuggestionsSectionSkeleton />}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <SuggestionsSectionSuspense videoId={videoId} isManual={isManual} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
 
-  return( 
+const SuggestionsSectionSkeleton = () => {
+  return (
     <>
-  <div className="hidden md:block space-y-3" >
-    {suggestions.pages.flatMap((page)=>page.items.map((video)=>(
-      <VideoRowCard
-       key={video.id}
-       data={video}
-       size="compact"
-      />
-    )))}
-    </div>
-    <div className="block md:hidden space-y-10">
-       {suggestions.pages.flatMap((page)=>page.items.map((video)=>(
-      <VideoGridCard
-       key={video.id}
-       data={video}
-      />
-    )))}
-    </div>
-    <InfiniteScroll
-    isManual={isManual}
-    hasNextPage={query.hasNextPage}
-    isFetchingNextPage={query.isFetchingNextPage}
-    fetchNextPage={query.fetchNextPage}
-    />
+      <div className="hidden md:block space-y-3">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <VideoRowCardSkeleton key={index} size="compact" />
+        ))}
+      </div>
+      <div className="block md:hidden space-y-10">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <VideoGridCardSkeleton key={index} />
+        ))}
+      </div>
     </>
-    )
+  );
+};
+const SuggestionsSectionSuspense = ({
+  videoId,
+  isManual,
+}: SuggestionSectionProps) => {
+  const [suggestions, query] =
+    trpc.suggestions.getMany.useSuspenseInfiniteQuery(
+      {
+        videoId,
+        limit: DEFAULT_LIMIT,
+      },
+      {
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+      }
+    );
+
+  return (
+    <>
+      <div className="hidden md:block space-y-3">
+        {suggestions.pages.flatMap((page) =>
+          page.items.map((video) => (
+            <VideoRowCard key={video.id} data={video} size="compact" />
+          ))
+        )}
+      </div>
+      <div className="block md:hidden space-y-10">
+        {suggestions.pages.flatMap((page) =>
+          page.items.map((video) => (
+            <VideoGridCard key={video.id} data={video} />
+          ))
+        )}
+      </div>
+      <InfiniteScroll
+        isManual={isManual}
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </>
+  );
 };
--- src/modules/videos/ui/views/video-view.tsx
+++ src/modules/videos/ui/views/video-view.tsx
@@ -1,5 +1,5 @@
 import { CommentsSection } from "../sections/comments-sections";
-import { SuggestionSection } from "../sections/suggestions-section";
+import { SuggestionsSection } from "../sections/suggestions-section";
 import { VideoSection } from "../sections/video-section";
 
 interface VideoViewProps {
@@ -13,12 +13,12 @@ export const VideoView = ({ videoId }: VideoViewProps) => {
         <div className="flex-1 min-w-0">
           <VideoSection videoId={videoId} />
           <div className="xl:hidden block mt-4">
-            <SuggestionSection videoId={videoId} isManual/>
+            <SuggestionsSection videoId={videoId} isManual />
           </div>
           <CommentsSection videoId={videoId} />
         </div>
         <div className="hidden xl:block w-full xl:w-[380px] 2xl:w-[460px] shrink-1">
-          <SuggestionSection videoId={videoId}/>
+          <SuggestionsSection videoId={videoId} />
         </div>
       </div>
     </div>
